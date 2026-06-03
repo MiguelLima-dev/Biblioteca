2cdd2b1633e56bb76d58e414c04752e42db2d863
Miguel Lima <miguelaugustolima07@gmail.com>
2026-06-03 12:24:02 -0300

    Programa carrega a planilha na memória.

--- teste.cpp
+++ teste.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 using namespace std;
 
+// Estrutura utilizada para armazenar as informações de cada livro
 struct livro {
     string titulo;
     int lancamento;
@@ -10,35 +11,57 @@ struct livro {
     char sexo; 
 };
 
+// Lê um livro do arquivo.
+ bool leitura(ifstream &arq, livro* vetor, int &indice) {
+    if (!getline(arq, vetor[indice].titulo, ','))
+        return false;
+
+    if (!arq >> vetor[indice].lancamento)
+        return false;
+    
+    char virgula; // Variável para o descarte da vírgula.
+    if (!arq >> virgula)
+        return false;
+
+    if(!getline(arq, vetor[indice].autor, ','))
+        return false;
+
+    if(!getline(arq, vetor[indice].genero, ','))
+        return false;
+
+    if (!arq >> vetor[indice].sexo)
+        return false;
+    arq.ignore();
+
+    indice++;
+    return true;
+}
+
 int main(){
-    ifstream bancoDados("biblioteca.csv");
-    ofstream saida("saida.txt");
-    livro biblioteca[40];
+    ifstream planilha("biblioteca.csv");
 
-    char virgula;
-    for (int i = 0; i < 1; i++){
-        getline(bancoDados, biblioteca[i].titulo, ',');
-        
-        bancoDados >> biblioteca[i].lancamento;
-        bancoDados >> virgula;
-
-        getline(bancoDados, biblioteca[i].autor, ',');
-
-        getline(bancoDados, biblioteca[i].genero, ',');
-
-        bancoDados >> biblioteca[i].sexo;
-        bancoDados.ignore();
+    if (!planilha) {
+        cout << "Erro ao abrir o arquivo.\n";
     }
-    bancoDados.close();
 
-    for (int i = 0; i < 1; i++){
-        saida << biblioteca[i].titulo;
-        saida << biblioteca[i].lancamento;
-        saida << biblioteca[i].autor;
-        saida << biblioteca[i].genero;
-        saida << biblioteca[i].sexo;
+    int capac = 40; // Quantas posiçoes o vetor guarda.
+    livro* vetor = new livro[capac];
+
+    char virgula; // Variável para descarte da vírgula.
+    int ocupados = 0; // Quantos livros foram inseridos.
+    while(leitura(planilha, vetor, ocupados)){   
+        if (ocupados == capac) { // Redimensiona o vetor. 
+            livro* novo = new livro[capac + 5];
+            for (int i = 0; i < capac; i++) {
+                novo[i] = vetor[i];
+            }
+            delete[] vetor;
+            vetor = novo;
+            capac += 5;
+        }
     }
-    saida.close();
+    planilha.close();
 
+    delete[] vetor;
     return 0;
 }
