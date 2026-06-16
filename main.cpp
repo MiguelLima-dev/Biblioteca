674f4def24578c51ce60524acd496562b130f871
Miguel Lima <miguelaugustolima07@gmail.com>
2026-06-16 15:22:58 -0300

    Adição de funcionalidades.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/* Ana Carolina Silva Sousa, Maria Clara, Miguel Augusto de Lima
+Turma: 14A
+Tema: Livros
+*/
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -30,11 +34,40 @@ bool leitura(ifstream &arq, livro &temp) {
     return true;
 }
  
+// Ordena os livros por título.
+// Particiona usando Hoare.
+int particao(livro v[], int inicio, int fim) {
+    livro pivo = v[inicio];
+    int i = inicio - 1, j = fim + 1;
+    while (i <= j) {
+        if (v[i].titulo <= pivo.titulo) i++;
+        else if (pivo.titulo <= v[j].titulo) j--;
+        else {
+            swap(v[i], v[j]);
+            i++;
+            j--;
+        }
+    }
+    v[inicio] = v[j];
+    v[j] = pivo;
+    return j;
+}
+// Executa o algoritmo de quick sort.
+void quickSort(livro vet[], int esq, int dir) {
+    if (esq < dir) {
+        int pivo = particao(vet, esq, dir);
+        quickSort(vet, esq, pivo - 1);
+        quickSort(vet, pivo + 1, dir);
+    }
+}
 // Executa busca binária, considerando o vetor ordenado por título.
 int buscaBinaria(livro* v, int inicio, int fim, string procurado) {
     if (inicio > fim) return -1;
     int meio = (inicio + fim) / 2;
-    if (procurado == v[meio].titulo) return meio;
+    if (procurado == v[meio].titulo) {
+        if (v[meio].ativo) return meio;
+        else return -1;
+    }
     if (procurado > v[meio].titulo) return buscaBinaria(v, meio + 1, fim, procurado);
     else return buscaBinaria(v, inicio, meio - 1, procurado);
 }
@@ -102,6 +135,56 @@ void mostraIntervalo(livro* v, int ocupados, int inicio, int fim) {
     }
 }
  
+// Inseri um livro no vetor.
+void insercao(livro* &v, int &tam, int &cap) {
+    if (tam == cap) {
+        livro* novo = new livro[cap + 1];
+        copy(v, v + cap, novo);
+        delete[] v;
+        v = novo;
+        cap++;
+    }
+    cout << "\n--- Cadastro de novos livros ---\n";
+	cout << "Insira o título do novo livro: ";
+    getline(cin, v[tam].titulo);
+
+    cout << "Insira o ano de lançamento da obra: ";
+	cin >> v[tam].lancamento;
+	cin.ignore();
+	
+	cout << "Insira o nome do autor(a): ";
+	getline(cin, v[tam].autor);
+	
+	cout << "Insira o genero do livro: ";
+	getline(cin, v[tam].genero);
+	
+	cout << "Insira o sexo do autor(a) (M/F): ";
+	cin >> v[tam].sexo;
+    cout << "Livro cadastrado com sucesso!\n";
+    
+    tam++;
+    quickSort(v, 0, tam - 1);
+}
+
+// Removi logicamente um livro do vetor.
+void deletar(livro v[], int tam, string titulo) {
+    int indice = buscaBinaria(v, 0, tam - 1, titulo);
+    if (indice != -1) {
+        v[indice].ativo = false;
+        cout << "Livro removido com sucesso!\n";
+    }
+}
+
+void salvar(ofstream &arq,livro v[], int tam) {
+    for (int i = 0; i < tam; i++) {
+        if (v[i].ativo) {
+        arq << "\"" << v[i].titulo << "\"" << "," <<
+        v[i].lancamento << "," << v[i].autor << "," <<
+        v[i].genero << "," << v[i].sexo << endl;
+        }
+    }
+}
+
 int main(){
     ifstream planilha("biblioteca.csv");
  
@@ -125,30 +208,19 @@ int main(){
         seja, aumenta o vetor em cinco espaços.*/
         if (ocupados == capac) {
             livro* novo = new livro[capac + 5];
-            for (int i = 0; i < capac; i++) {
-                novo[i] = vetor[i];
-            }
+            copy(vetor, vetor + ocupados, novo);
             delete[] vetor;
             vetor = novo;
             capac += 5;
         }
- 
-        /* Insere os livro no vetor, já ordenados por título. A comparação utilizada
-        é o código ASCII(padrão na comparação de strings em C++)*/ 
-        if (ocupados == 0)
-            vetor[0] = temp;
-        else {
-            int j = 0;
-            while (j < ocupados && temp.titulo > vetor[j].titulo)
-                j++;
-            for (int k = ocupados; k > j; k--)
-                vetor[k] = vetor[k - 1];
-            vetor[j] = temp;
-        }
+        
+        vetor[ocupados] = temp;
         ocupados++;
     }
     planilha.close();
  
+    quickSort(vetor, 0, ocupados - 1);
+
     /* Menu principal. O usuário escolhe a operação desejada e o programa
     executa até que a opção de saída seja escolhida. */
     int opcao;
@@ -223,4 +295,3 @@ int main(){
     delete[] vetor;
     return 0;
 }
- 
